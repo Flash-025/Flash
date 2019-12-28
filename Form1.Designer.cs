9172247e23d763e54a25d87719d3252d4049208c
FlashSherry <57252508+FlashSherry@users.noreply.github.com>
2019-12-28 14:45:59 +0500

    Project  Final

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace myAssistant1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnEnableVoice = new System.Windows.Forms.Button();
+            this.btnDisableVoice = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.voiceBtn = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // btnEnableVoice
+            // 
+            this.btnEnableVoice.AccessibleName = "";
+            this.btnEnableVoice.BackColor = System.Drawing.Color.White;
+            this.btnEnableVoice.ForeColor = System.Drawing.Color.Black;
+            this.btnEnableVoice.ImageIndex = 3;
+            this.btnEnableVoice.ImageList = this.voiceBtn;
+            this.btnEnableVoice.Location = new System.Drawing.Point(31, 214);
+            this.btnEnableVoice.Name = "btnEnableVoice";
+            this.btnEnableVoice.Size = new System.Drawing.Size(58, 61);
+            this.btnEnableVoice.TabIndex = 1;
+            this.btnEnableVoice.UseVisualStyleBackColor = false;
+            this.btnEnableVoice.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDisableVoice
+            // 
+            this.btnDisableVoice.AccessibleName = "";
+            this.btnDisableVoice.BackColor = System.Drawing.Color.White;
+            this.btnDisableVoice.Enabled = false;
+            this.btnDisableVoice.ForeColor = System.Drawing.Color.Black;
+            this.btnDisableVoice.ImageIndex = 0;
+            this.btnDisableVoice.ImageList = this.voiceBtn;
+            this.btnDisableVoice.Location = new System.Drawing.Point(476, 214);
+            this.btnDisableVoice.Name = "btnDisableVoice";
+            this.btnDisableVoice.Size = new System.Drawing.Size(58, 61);
+            this.btnDisableVoice.TabIndex = 1;
+            this.btnDisableVoice.UseVisualStyleBackColor = false;
+            this.btnDisableVoice.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Maroon;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(553, 95);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // voiceBtn
+            // 
+            this.voiceBtn.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("voiceBtn.ImageStream")));
+            this.voiceBtn.TransparentColor = System.Drawing.Color.Transparent;
+            this.voiceBtn.Images.SetKeyName(0, "mute-microphone-interface-symbol.png");
+            this.voiceBtn.Images.SetKeyName(1, "mic.png");
+            this.voiceBtn.Images.SetKeyName(2, "voice-control.png");
+            this.voiceBtn.Images.SetKeyName(3, "voice-recorder-microphone.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(577, 340);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnDisableVoice);
+            this.Controls.Add(this.btnEnableVoice);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnableVoice;
+        private System.Windows.Forms.Button btnDisableVoice;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ImageList voiceBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace myAssistant1
+{
+   
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer flash = new SpeechSynthesizer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new String[] 
+            {
+                "open My H C I Lecture From D Drive ",
+                " say Hello",
+                " what Time Is It ",
+                " stop Listening",
+                
+                " google Please ",
+                "face Book Please",
+                "open My mail Account",
+                "mini Mize Window",
+                "maxi Mize Window",
+                "normalize Window Size",
+                "you Tube Please",
+                "open Song Folder"
+
+            });
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
+            Grammar grammar = new Grammar(gBuilder);
+
+            recEngine.LoadGrammarAsync(grammar);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += RecEngine_SpeechRecognized;
+
+
+        }
+
+        private void RecEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speech = e.Result.Text;
+            
+
+            if (e.Result.Text == "open My H C I Lecture From D Drive")
+            {
+                richTextBox1.Text = "YOU : " + speech;
+                System.Diagnostics.Process.Start("explorer.exe", @"D:\BSE-V\HCI\class\Lect 1 HCI Introduction 12.09.17.ppt");
+                //MessageBox.Show("\nHello Sir. How are you");
+               
+            }
+            else if (e.Result.Text == "say Hello")
+            {
+                richTextBox1.Text = "YOU : " + speech;
+                flash.SpeakAsync("Hello Boss How are you");
+
+
+                MessageBox.Show("\nHello Boss How are you");
+                
+
+            }
+            else if (e.Result.Text == "what Time Is It")
+            {
+                richTextBox1.Text = "YOU : " + speech;
+                richTextBox1.Text = "FLASH : " + speech;
+                flash.SpeakAsync(DateTime.Now.ToString("h mm tt"));
+
+
+            }
+            else if (e.Result.Text == "stop Listening")
+            {
+                richTextBox1.Text = "YOU : " + speech;
+                flash.SpeakAsync("ok Boss.I Am In Sleep Mode");
+                recEngine.RecognizeAsyncStop();
+                btnDisableVoice.Enabled = false;
+
+            }
+            //else if (e.Result.Text == "start Listening")
+            //{
+            //    richTextBox1.Text = "YOU : " + speech;
+            //    flash.SpeakAsync("welcome Boss I Am In Ready");
+            //    recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            //    btnDisableVoice.Enabled = true;
+
+            //}
+            else if (e.Result.Text == "google Please")
+            {
+                richTextBox1.Text = "YOU : " + speech;
+                System.Diagnostics.Process.Start("https://www.google.com/?gws_rd=ssl");
+
+            }
+            else if (e.Result.Text == "face Book Please")
+            {
+                richTextBox1.Text = "YOU : " + speech;
+                System.Diagnostics.Process.Start("https://www.facebook.com/");
+            
+
+            }
+            else if (e.Result.Text == "open My mail Account")
+            {
+                richTextBox1.Text = "YOU : " + speech;
+                System.Diagnostics.Process.Start("explorer.exe", @"C:\Program Files\WindowsApps\Microsoft.Office.Desktop.Outlook_16051.12228.20364.0_x86__8wekyb3d8bbwe\Office16\OUTLOOK.EXE");
+
+
+            }
+            else if (e.Result.Text == "mini Mize Window")
+            {
+                richTextBox1.Text = "YOU : " + speech;
+                this.WindowState = FormWindowState.Minimized;
+
+
+            }
+            else if (e.Result.Text == "maxi Mize Window")
+            {
+                richTextBox1.Text = "YOU : " + speech;
+                this.WindowState = FormWindowState.Maximized;
+
+
+            }
+            else if (e.Result.Text == "normalize Window Size")
+            {
+                richTextBox1.Text = "YOU : " + speech;
+                this.WindowState = FormWindowState.Normal;
+
+            }
+            else if (e.Result.Text == "stop Debugging")
+            {
+                richTextBox1.Text = "YOU : " + speech;
+                
+
+            }
+            else if (e.Result.Text == "you Tube Please")
+            {
+                richTextBox1.Text = "YOU : " + speech;
+                System.Diagnostics.Process.Start("https://www.youtube.com/");
+            
+
+            }
+
+            else if (e.Result.Text == "open Song Folder")
+            {
+
+                richTextBox1.Text = "YOU : " + speech;
+                System.Diagnostics.Process.Start("explorer.exe", @"C:\Users\Sherry\Desktop\gaany");
+
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            btnDisableVoice.Enabled = true;
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            btnDisableVoice.Enabled = false;
+        }
+    }
+}
